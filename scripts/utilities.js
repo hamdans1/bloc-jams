7f5b1afa413c673856f2a95d9a9cb4ed39ef3100
hamdans1 <samihamdan00@gmail.com>
2017-03-10 07:09:51 -0500

    updated checkpoint9

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,12 +1,6 @@
-var pointsArray = document.getElementsByClassName('point');
-
-var specialCallback = function (anything) {
-    console.log(anything + "is a point");
-};
-
 function forEach(array, callbackArgument){
     for (var i=0; i<array.length; i++){
-        specialCallback(point[i]);
+        callbackArgument(array[i]);
     }
 }
 
